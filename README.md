c9696a899f14215f251d137b62565f1b17fec48d
Anders Steen <110468350+andRes-steen@users.noreply.github.com>
2022-11-11 23:10:29 +0100

    add documented example code

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# user-study-interface
- 
+# Procedural generation user study example
+
+This is the interface I used for conducting a user study as part of my [master's thesis](http://kth.diva-portal.org/smash/record.jsf?pid=diva2%3A1661985&dswid=9843). It implements a 2 alternative forced choice (2AFC) study design to find perceived differences between images generated procedurally. See the method described in detail in section 4 of the thesis.
+
+You may modify and use the code for your own project, without attribution or anything.
+
+## How to use
+
+Fork the repo and make your necessary modifications. Read below for a general explanation of how to use it.
+
+### Intro
+
+The participant will first give their consent to partake, then get general instructions, then get to see some reference images to better understand what to look for and compare against. Of these sections, it is recommended to keep the consent close to the way it is, with details changed. The instructions and reference images should be changed according to your needs.
+
+### Image comparison
+
+Images are compared in pairs as specified in the code. Replace the images and the pair configurations with whatever you need. Potentially look at changing the text in the comparison part to suit your needs. In order for the images to load fast when they need to be displayed, they are all fetched when the site is originally loaded, see the bottom of index.html.
+
+### Demographics
+
+Only collect the data you need from users, both whatever data is necessary to analyze the results and come to your conclusions, but also data needed to make sure that the participants are well distributed within the group you're testing. For instance, I did not use the gender data for analysis, only to make sure the participants were evenly distributed and could represent their group.
+
+### Data collection
+
+In each trial, the data saved consists of the name of the selected image, the name of the non-selected image, and whether the selected image was on the left or right. This data from all the trials is collected in an array in the order the trials were in, and combined with the demographic data in a JS object and serialized into JSON.
+
+The action of the data collection form in index.html links to a Google Apps Script that saves the data into a Google Sheet. See the files google/exampleResults.xlsx and google/exampleScript.gs. [This site](https://www.benlcollins.com/spreadsheets/saving-data-in-google-sheets/) seems to explain it pretty well.
---
+++ index.js
@@ -0,0 +1,197 @@
+const trials = [
+    ["S1C1C1", "S1C1C2"],
+    ["S1C1C1", "S1C1C3"],
+    ["S1C1C1", "S1C2C1"],
+    ["S1C1C1", "S1C3C1"],
+    ["S1C1C1", "S2C1C1"],
+    ["S1C1C1", "S3C1C1"],
+    ["S1C1C2", "S1C1C3"],
+    ["S1C1C2", "S1C2C2"],
+    ["S1C1C2", "S1C3C2"],
+    ["S1C1C2", "S2C1C2"],
+    ["S1C1C2", "S3C1C2"],
+    ["S1C1C3", "S1C2C3"],
+    ["S1C1C3", "S1C3C3"],
+    ["S1C1C3", "S2C1C3"],
+    ["S1C1C3", "S3C1C3"],
+    ["S1C2C1", "S1C2C2"],
+    ["S1C2C1", "S1C2C3"],
+    ["S1C2C1", "S1C3C1"],
+    ["S1C2C1", "S2C2C1"],
+    ["S1C2C1", "S3C2C1"],
+    ["S1C2C2", "S1C2C3"],
+    ["S1C2C2", "S1C3C2"],
+    ["S1C2C2", "S2C2C2"],
+    ["S1C2C2", "S3C2C2"],
+    ["S1C2C3", "S1C3C3"],
+    ["S1C2C3", "S2C2C3"],
+    ["S1C2C3", "S3C2C3"],
+    ["S1C3C1", "S1C3C2"],
+    ["S1C3C1", "S1C3C3"],
+    ["S1C3C1", "S2C3C1"],
+    ["S1C3C1", "S3C3C1"],
+    ["S1C3C2", "S1C3C3"],
+    ["S1C3C2", "S2C3C2"],
+    ["S1C3C2", "S3C3C2"],
+    ["S1C3C3", "S2C3C3"],
+    ["S2C1C1", "S2C1C2"],
+    ["S2C1C1", "S2C1C3"],
+    ["S2C1C1", "S2C2C1"],
+    ["S2C1C1", "S2C3C1"],
+    ["S2C1C1", "S3C1C1"],
+    ["S2C1C2", "S2C1C3"],
+    ["S2C1C2", "S2C2C2"],
+    ["S2C1C2", "S2C3C2"],
+    ["S2C1C2", "S3C1C2"],
+    ["S2C1C3", "S2C2C3"],
+    ["S2C1C3", "S2C3C3"],
+    ["S2C1C3", "S3C1C3"],
+    ["S2C2C1", "S2C2C2"],
+    ["S2C2C1", "S2C2C3"],
+    ["S2C2C1", "S2C3C1"],
+    ["S2C2C1", "S3C2C1"],
+    ["S2C2C2", "S2C2C3"],
+    ["S2C2C2", "S2C3C2"],
+    ["S2C2C2", "S3C2C2"],
+    ["S2C2C3", "S2C3C3"],
+    ["S2C2C3", "S3C2C3"],
+    ["S2C3C1", "S2C3C2"],
+    ["S2C3C1", "S2C3C3"],
+    ["S2C3C1", "S3C3C1"],
+    ["S2C3C2", "S2C3C3"],
+    ["S2C3C2", "S3C3C2"],
+    ["S3C1C1", "S3C1C2"],
+    ["S3C1C1", "S3C1C3"],
+    ["S3C1C1", "S3C2C1"],
+    ["S3C1C1", "S3C3C1"],
+    ["S3C1C2", "S3C1C3"],
+    ["S3C1C2", "S3C2C2"],
+    ["S3C1C2", "S3C3C2"],
+    ["S3C1C3", "S3C2C3"],
+    ["S3C2C1", "S3C2C2"],
+    ["S3C2C1", "S3C2C3"],
+    ["S3C2C1", "S3C3C1"],
+    ["S3C2C2", "S3C2C3"],
+    ["S3C2C2", "S3C3C2"],
+    ["S3C3C1", "S3C3C2"]
+];
+const trialOrder = [...trials.keys()];
+shuffleArray(trialOrder);
+const numTrials = trials.length;
+let currentTrialIndex = -1;
+const pageDelay = 5000;
+const trialDelay = 1000;
+let startTime = new Date();
+let results = []
+
+setTimeout(() => $("#continueButton").prop("disabled", false), pageDelay);
+
+if (window.screen.height < 600 || window.screen.width < 1000) {
+    $("#consentPage").hide();
+    $("#screenSizeWarning").show();
+    $("#screenSize").text(`Your current screen size is ${window.screen.width}x${window.screen.height}.`);
+}
+
+function continueButtonPressed() {
+    $("#consentPage").hide();
+    $("#instructionPage").show();
+    $("#welcomeHeading").hide();
+    setTimeout(() => $("#startButton").prop("disabled", false), pageDelay);
+}
+
+function startButtonPressed() {
+    $("#instructionPage").hide();
+    $("#referencePage").show();
+    setTimeout(() => $("#trialStartButton").prop("disabled", false), pageDelay);
+}
+
+function trialStartButtonPressed() {
+    $("#referencePage").hide();
+    startTrials();
+}
+
+function startTrials() {
+    $("#trialPage").show();
+    setButtonEnableTimer("realismButton", trialDelay);
+    nextTrial();
+}
+
+function realismSubmit(button) {
+    setButtonEnableTimer("realismButton", trialDelay);
+    results.push(getTrialResult(button));
+    nextTrial();
+}
+
+function getTrialResult(button) {
+    let imageString = "";
+    if (button === "Left") {
+        imageString = $("#leftImage").attr("src");
+    } else {
+        imageString = $("#rightImage").attr("src");
+    }
+    return {
+        trialNum: trialOrder[currentTrialIndex],
+        selectedImage: imageString.substring(4, imageString.length - 4),
+        selectedButton: button
+    }
+}
+
+function nextTrial() {
+    currentTrialIndex += 1;
+    if (currentTrialIndex >= numTrials) {
+        finishTrials();
+    } else {
+        $("#trialNumber").text(`Trial ${currentTrialIndex + 1}`)
+        if (Math.random() < 0.5) {
+            $("#leftImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][0]}.png`);
+            $("#rightImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][1]}.png`);
+        } else {
+            $("#leftImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][1]}.png`);
+            $("#rightImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][0]}.png`);
+        }
+    }
+}
+
+function finishTrials() {
+    $("#trialPage").hide();
+    $("#demographicsPage").show();
+}
+
+function verifyAndGatherData() {
+    let potentialAge  = parseInt($("input[name=age]").val(), 10);
+    let potentialCountry = $("input[name=country]").val();
+    
+    if (Number.isInteger(potentialAge) && potentialCountry) {
+        let data = {
+            gender: $("select[name=gender]").find(":selected").text(),
+            age: potentialAge,
+            education: $("select[name=education]").find(":selected").text(),
+            country: potentialCountry,
+            experience: $("select[name=experience]").find(":selected").text(),
+            vision: $("select[name=vision]").find(":selected").text(),
+            comments: $("textarea[name=comments]").val(),
+            duration: 0.001 * (new Date() - startTime),
+            trialResults: results
+        }
+
+        $("input[name=Data]").val(JSON.stringify(data));
+        $("#dataForm").submit();
+        $("#demographicsPage").hide();
+    } else {
+        alert("Incorrect data, correct any errors and try again.");
+    }
+}
+
+function setButtonEnableTimer(className, delay) {
+    $(`.${className}`).prop("disabled", true);
+    setTimeout(() => $(`.${className}`).prop("disabled", false), delay);
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+}
